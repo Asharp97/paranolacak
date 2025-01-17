756efa10b7dbaf141b85b1d40ca8c01e75356f1e
Ali Elsayed <ali-hisham@hotmail.com>
2025-01-17 22:01:10 +0300

    file-upload memory fix

--- components/file-upload.vue
+++ components/file-upload.vue
@@ -5,19 +5,13 @@
       type="file"
       style="display: none"
       accept="image/png, image/jpg, image/jpeg, image/pdf, image/svg, image/webp"
-      @change="imageSelected"
-    />
+      @change="select" />
     <button
       class="file-upload-area"
-      @click="$refs.imageUploadReference.click()"
-    >
+      @click="$refs.imageUploadReference.click()">
       <Icon name="lets-icons:upload" class="icon" />
       <p>Yükle</p>
     </button>
-    <div v-if="imgName" class="file-name">
-      <p>{{ imgName }}</p>
-      <Icon name="material-symbols:delete-rounded" class="icon" @click="clearImage" />
-    </div>
     <div v-if="error" class="errormessage">
       {{ error }}
     </div>
@@ -25,24 +19,19 @@
 </template>
 
 <script setup>
-const emit = defineEmits(["sendImg"]);
+const emit = defineEmits(["sendImage"]);
+defineProps(["selectedImage"]);
 const error = ref(null);
-let imgFile;
+const file = ref();
 // let imgExt;
-const imgName = ref();
-const imageSelected = (event) => {
+const select = (event) => {
   if (event.target.files[0].type.startsWith("image/")) {
     error.value = null;
-    imgFile = event.target.files[0];
-    imgName.value = event.target.files[0].name;
-    emit("sendImg", imgName.value, imgFile);
+    file.value = event.target.files[0];
+    emit("sendImage", file.value);
   } else error.value = "Resmin formate olması gerekiyor";
 };
 
-const clearImage = () => {
-  imgFile = null;
-  imgName.value = null;
-};
 </script>
 
 <style lang="scss" scoped>
@@ -60,15 +49,6 @@ const clearImage = () => {
     padding-block: 1.8rem;
     .icon {
       font-size: 3rem;
-    }
-  }
-  .file-name {
-    display: flex;
-    margin-block: 1rem;
-    align-items: center;
-    justify-content: space-between;
-    .icon {
-      cursor: pointer;
     }
   }
 }
--- components/kimlik-foto.vue
+++ components/kimlik-foto.vue
@@ -4,17 +4,38 @@
       <auth-steps :step="activeStep" @activate-step="activateStep" />
       <div v-if="activeStep == 1">
         <h6 class="title">Lütfen özçekim fotoğrafınızı yükleyin.</h6>
-        <File-upload @send-image="recieveImage" />
+        <File-upload @send-image="(name) => handleFileChange(name, 0)" />
+        <div v-if="fileNames[0]" class="file-name">
+          {{ fileNames[0].name }}
+          <Icon
+            name="material-symbols:delete-rounded"
+            class="icon"
+            @click="clearImage(0)" />
+        </div>
       </div>
       <div v-if="activeStep == 2">
         <h6 class="title">
           Lütfen T.C. Kimlik kartınızın arka yüz fotoğrafını yükleyin.
         </h6>
-        <File-upload @send-image="recieveImage" />
+        <File-upload @send-image="(name) => handleFileChange(name, 1)" />
+        <div v-if="fileNames[1]" class="file-name">
+          {{ fileNames[1].name }}
+          <Icon
+            name="material-symbols:delete-rounded"
+            class="icon"
+            @click="clearImage(1)" />
+        </div>
       </div>
       <div v-if="activeStep == 3">
         <h6 class="title">Lütfen özçekim fotoğrafınızı yükleyin.</h6>
-        <File-upload @send-image="recieveImage" />
+        <File-upload @send-image="(name) => handleFileChange(name, 2)" />
+        <div v-if="fileNames[2]" class="file-name">
+          {{ fileNames[2].name }}
+          <Icon
+            name="material-symbols:delete-rounded"
+            class="icon"
+            @click="clearImage(2)" />
+        </div>
       </div>
       <div v-if="activeStep == 4">
         <h6 class="title">Kimlik doğrulama işleminiz kontrol ediliyor.</h6>
@@ -27,17 +48,37 @@
         v-if="activeStep <= 3"
         text="Devam Et"
         variant="fill"
-        @click="activeStep++"
-      />
+        @click="activeStep++" />
     </TransitionGroup>
   </Auth>
 </template>
 
 <script setup>
 const activeStep = ref(1);
-const activateStep = (x) => (activeStep.value = x);
-const recieveImage = (name, file) =>
-  console.log("imgfile is: " + name + " " + file);
+const activateStep = (x) => {
+  if (fileNames.value[activeStep.value - 1] || x < activeStep.value)
+    activeStep.value = x;
+  else console.log("feed back error animation");
+};
+
+const fileNames = ref(["", "", ""]);
+const handleFileChange = (fileName, stepIndex) => {
+  fileNames.value[stepIndex] = fileName;
+};
+const clearImage = (stepIndex) => {
+  fileNames.value[stepIndex] = null;
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.file-name {
+  display: flex;
+  margin-block: 1rem;
+  align-items: center;
+  justify-content: space-between;
+  .icon {
+    cursor: pointer;
+    font-size: 1.8rem;
+  }
+}
+</style>
